e9c168205e20744e08ced5bc87722e8428671358
parinay11 <bhar0058@algonquinlive.com>
2024-02-01 17:37:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/parinaylab1/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.parinaylab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText    myedit=findViewById(R.id.myedittext);
+        Button mybutton=findViewById(R.id.mybutton);
+        TextView mytext= findViewById(R.id.textview);
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTextContent = myedit.getText().toString();
+                mytext.setText("Your Name Is: " + editTextContent);
+            }
+
+        });
+    }
+}
